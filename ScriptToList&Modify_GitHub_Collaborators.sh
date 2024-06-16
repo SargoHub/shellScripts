6fe880fc829c63e6407c3e1f086a38da110fde13
SargoHub <167636353+SargoHub@users.noreply.github.com>
2024-06-16 13:53:09 +0530

    Create ScriptToList&Modify_GitHub_Collaborators.sh

---
+++ ScriptToList&Modify_GitHub_Collaborators.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#GitHub API URL
+API_URL="https://api.github.com"
+
+#Github Username and personal access token
+USERNAME=$username
+TOKEN=$token
+
+#Owner/organisatio and Repository information 
+REPO_OWNER=$1
+REPO_NAME=$2
+
+
